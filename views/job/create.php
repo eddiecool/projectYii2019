178dc7ed69b782c09e2e23bd274308cc982ff587
Eddie Chan <eddie@hobbydigi.com>
2019-08-11 15:08:51 +0800

    customize job form input

--- views/job/create.php
+++ views/job/create.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\models\Category;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $job app\models\Job */
@@ -12,18 +14,36 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-        <?= $form->field($job, 'category_id') ?>
+        <?= $form->field($job, 'category_id')->dropDownList(
+              category::find()
+              ->select(['name','id'])
+              ->indexBy('id')
+              ->column(), ['prompt' => 'Select Category']
+        ); ?>
         <?= $form->field($job, 'user_id') ?>
         <?= $form->field($job, 'title') ?>
-        <?= $form->field($job, 'description') ?>
-        <?= $form->field($job, 'type') ?>
+        <?= $form->field($job, 'description')->textArea(['rows' => '5'])
+        ?>
+        <?= $form->field($job, 'type') ->dropDownList([
+                'full_time' => 'Full Time',
+                'part_time' => 'Part Time',
+                'as_needed' => 'As Needed'
+        ], ['prompt' => 'Select Category' ]); ?>
         <?= $form->field($job, 'requirements') ?>
-        <?= $form->field($job, 'salary_range') ?>
+        <?= $form->field($job, 'salary_range')->dropDownList([
+            'Under $1000' => 'Under $1000',
+            '$1000 - $2000' => '$1000 - $2000',
+            '$2000 - $4000' => '$2000 - $4000',
+            '$4000 - $6000' => '$4000 - $6000',
+        ], ['prompt' => 'Select Salary Range' ]); ?>
         <?= $form->field($job, 'city') ?>
         <?= $form->field($job, 'address') ?>
         <?= $form->field($job, 'contact_email') ?>
         <?= $form->field($job, 'contact_phone') ?>
-        <?= $form->field($job, 'is_published') ?>
+        <?= $form->field($job, 'is_published')
+        ->radioList(['1' => 'Yes', '0' => 'No']);
+
+        ?>
         <?= $form->field($job, 'create_date') ?>
     
         <div class="form-group">
